ea67f5c28cda255894d77da88288c5ba6c467f4a
komeil <catinrage@gmail.com>
2025-05-29 20:39:41 +0330

    Refactor Builder class to improve file management and command options
    - Changed `BuilderOptions` and `FileOptions` from types to interfaces for better extensibility.
    - Introduced a `type` property in the `File` class to distinguish between 'Main' and 'Sub' files.
    - Updated the `put` method to utilize the current file context more effectively, enhancing command generation.
    - Added new methods for setting machine plane and unit systems, improving G-code generation capabilities.

--- src/lib/builder.ts
+++ src/lib/builder.ts
@@ -1,3 +1,4 @@
+import type { PlanEnum } from '$src/common/enums';
 import type { CommandsType, DeepPartial, EventsType } from '$src/types';
 import { Machine } from './machine';
 import type { Event } from './program';
@@ -19,7 +20,7 @@ export interface CommandOptions {
 /**
  * @description Configuration options for the Builder class
  */
-export type BuilderOptions = {
+export interface BuilderOptions {
   /**
    * The name of the main file, default is 'Setup.MPF'
    */
@@ -41,11 +42,11 @@ export type BuilderOptions = {
      */
     increment: number;
   };
-};
+}
 
-export type FileOptions = {
+export interface FileOptions {
   numbering: BuilderOptions['numbering'];
-};
+}
 
 export class File {
   /**
@@ -99,9 +100,11 @@ export class File {
   constructor(
     builder: Builder,
     name: string,
+    type: 'Main' | 'Sub' = 'Sub',
     options?: DeepPartial<FileOptions>,
   ) {
     this.name = name;
+    this.type = type;
     this._builder = builder;
     this._options = {
       numbering: {
@@ -121,6 +124,13 @@ export class File {
   public readonly name: string;
 
   /**
+   * @public
+   * @property type
+   * @description The type of the file.
+   */
+  public readonly type: 'Main' | 'Sub';
+
+  /**
    * @property {string} gcode
    * @description Returns the complete G-code program as a single string,
    * with individual G-code lines joined by newline characters.
@@ -135,14 +145,14 @@ export class File {
    * @description Adds a G-code word or segment (e.g., "G00", "X100", "M03") to the current line being built.
    * Sections should be valid G-code words. Empty strings will be ignored by `flush`.
    * @param {string} section - The G-code segment to add to the current line.
-   * @param {boolean} [flush] - Whether to flush the current line after adding the section.
+   * @param {boolean} [skipNewLine] - Whether to skip a new line after adding the section.
    */
-  public put(section: string, flush?: boolean): void {
+  public put(section: string, skipNewLine?: boolean): void {
     if (section && section.trim().length > 0) {
       // Ensure non-empty sections are added
       this._currentLine.push(section.trim());
     }
-    if (flush) {
+    if (!skipNewLine) {
       this.flush();
     }
   }
@@ -196,20 +206,6 @@ export class Builder {
 
   /**
    * @private
-   * @property _mainFile
-   * @description The main file.
-   */
-  private _mainFile: File;
-
-  /**
-   * @private
-   * @property _currentFile
-   * @description The current file.
-   */
-  private _currentFile: File;
-
-  /**
-   * @private
    * @property _machine
    * @description An instance of the Machine class to manage CNC machine state and generate G-code words.
    */
@@ -230,11 +226,25 @@ export class Builder {
           options?.numbering?.increment ?? this._options.numbering.increment,
       },
     };
-    this._mainFile = new File(this, this._options.mainFileName);
-    this._files.push(this._mainFile);
-    this._currentFile = this._mainFile;
+    this.mainFile = new File(this, this._options.mainFileName, 'Main');
+    this._files.push(this.mainFile);
+    this.currentFile = this.mainFile;
     this._machine = new Machine(this);
   }
+
+  /**
+   * @public
+   * @property mainFile
+   * @description The main file.
+   */
+  public mainFile: File;
+
+  /**
+   * @public
+   * @property currentFile
+   * @description The current file.
+   */
+  public currentFile: File;
 
   /**
    * @public
@@ -273,20 +283,20 @@ export class Builder {
    * @param {CommandOptions} [options] - Optional settings for command generation.
    */
   public put(section: string, options?: CommandOptions): void {
-    this._currentFile.put(section, options?.forcePrint);
-    if (options?.skipNewLine) {
-      this._currentFile.flush();
+    this.currentFile.put(section, options?.skipNewLine);
+    if (!options?.skipNewLine) {
+      this.currentFile.flush();
     }
   }
 
   /**
    * @method flush
-   * @description Finalizes the current G-code line. If the `_currentLine` buffer contains any words,
+   * @description Finalizes the current G-code line in the current file. If the `_currentLine` buffer contains any words,
    * it prepends the line number (N-word), joins the words with spaces, trims whitespace,
    * adds it to the `_lines` array, clears the `_currentLine` buffer, and increments the `_lineNumber`.
    */
   public flush(): void {
-    this._currentFile.flush();
+    this.currentFile.flush();
   }
 
   /**
@@ -297,11 +307,11 @@ export class Builder {
    */
   public NewSubProgram(name: string): void {
     // If builder is not currently in the main file, throw and error as its only possible to create new files in the main file
-    if (this._currentFile !== this._mainFile) {
+    if (this.currentFile !== this.mainFile) {
       throw new Error('Cannot create new files in non-main (MPF) files');
     }
     this._files.push(new File(this, name + '.SPF'));
-    this._currentFile = this._files[this._files.length - 1];
+    this.currentFile = this._files[this._files.length - 1];
   }
 
   /**
@@ -311,11 +321,11 @@ export class Builder {
    */
   public EndSubProgram(): void {
     // If builder is currently in the main file, throw and error as its only possible to end the main file
-    if (this._currentFile === this._mainFile) {
+    if (this.currentFile === this.mainFile) {
       throw new Error('Cannot end the main (MPF) file');
     }
 
-    this._currentFile = this._mainFile;
+    this.currentFile = this.mainFile;
   }
 
   /**
@@ -348,8 +358,11 @@ export class Builder {
    * @example 'G0 X100 Y50 Z10'
    */
   public Rapid(params: CommandsType['Rapid'], options?: CommandOptions): void {
-    this.put(this._machine.setMotionMode(0, options?.forcePrint));
-    this.put(this._machine.setPosition(params, options?.forcePrint));
+    this.put(this._machine.setMotionMode(0, options?.forcePrint), {
+      skipNewLine: options?.skipNewLine ?? true,
+      forcePrint: options?.forcePrint,
+    });
+    this.put(this._machine.setPosition(params, options?.forcePrint), options);
   }
 
   /**
@@ -362,8 +375,27 @@ export class Builder {
    * @example 'G1 X100 Y50 Z10'
    */
   public Line(params: CommandsType['Line'], options?: CommandOptions): void {
-    this.put(this._machine.setMotionMode(1, options?.forcePrint));
-    this.put(this._machine.setPosition(params, options?.forcePrint));
+    this.put(this._machine.setMotionMode(1, options?.forcePrint), {
+      skipNewLine: options?.skipNewLine ?? true,
+      forcePrint: options?.forcePrint,
+    });
+    this.put(this._machine.setPosition(params, options?.forcePrint), options);
+  }
+
+  /**
+   * @method SetMachinePlane
+   * @description Generates G-code to set the machine's plane (G17, G18, G19).
+   * @param {PlanEnum} plane - The desired machine plane.
+   * @param {CommandOptions} [options] - Optional settings for command generation.
+   */
+  public SetMachinePlane(
+    plane: CommandsType['SetMachinePlane'],
+    options?: CommandOptions,
+  ): void {
+    this.put(
+      this._machine.setMachinePlane(plane, options?.forcePrint),
+      options,
+    );
   }
 
   /**
@@ -376,7 +408,10 @@ export class Builder {
     speed: CommandsType['SetSpindleSpeed'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.setSpindleSpeed(speed, options?.forcePrint));
+    this.put(
+      this._machine.setSpindleSpeed(speed, options?.forcePrint),
+      options,
+    );
   }
 
   /**
@@ -390,7 +425,10 @@ export class Builder {
     direction: CommandsType['SetSpindleDirection'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.setSpindleDirection(direction, options?.forcePrint));
+    this.put(
+      this._machine.setSpindleDirection(direction, options?.forcePrint),
+      options,
+    );
   }
 
   /**
@@ -404,7 +442,7 @@ export class Builder {
     feedRate: CommandsType['SetFeedRate'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.setFeedRate(feedRate, options?.forcePrint));
+    this.put(this._machine.setFeedRate(feedRate, options?.forcePrint), options);
   }
 
   /**
@@ -418,7 +456,10 @@ export class Builder {
     toolNumber: CommandsType['SelectTool'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.selectTool(toolNumber, options?.forcePrint));
+    this.put(
+      this._machine.selectTool(toolNumber, options?.forcePrint),
+      options,
+    );
   }
 
   /**
@@ -427,7 +468,25 @@ export class Builder {
    * @param {CommandOptions} [options] - Optional settings for command generation.
    */
   public ChangeTool(options?: CommandOptions): void {
-    this.put('M6');
+    this.put('M6', options);
+  }
+
+  /**
+   * @method UseMillimeters
+   * @description Generates G-code to set the machine to use millimeters (G710).
+   * @param {CommandOptions} [options] - Optional settings for command generation.
+   */
+  public UseMillimeters(options?: CommandOptions): void {
+    this.put(this._machine.setUnitSystem(710, options?.forcePrint), options);
+  }
+
+  /**
+   * @method UseInches
+   * @description Generates G-code to set the machine to use inches (G700).
+   * @param {CommandOptions} [options] - Optional settings for command generation.
+   */
+  public UseInches(options?: CommandOptions): void {
+    this.put(this._machine.setUnitSystem(700, options?.forcePrint), options);
   }
 
   /**
@@ -436,7 +495,10 @@ export class Builder {
    * @param {CommandOptions} [options] - Optional settings for command generation.
    */
   public SetAbsoluteMode(options?: CommandOptions): void {
-    this.put(this._machine.setPositioningMode(90, options?.forcePrint));
+    this.put(
+      this._machine.setPositioningMode(90, options?.forcePrint),
+      options,
+    );
   }
 
   /**
@@ -445,24 +507,19 @@ export class Builder {
    * @param {CommandOptions} [options] - Optional settings for command generation.
    */
   public SetIncrementalMode(options?: CommandOptions): void {
-    this.put(this._machine.setPositioningMode(91, options?.forcePrint));
+    this.put(
+      this._machine.setPositioningMode(91, options?.forcePrint),
+      options,
+    );
   }
 
   /**
-   * @method UseMillimeters
-   * @description Generates G-code to set the machine to use millimeters (G21).
+   * @method SetFeedRateMode
+   * @description Generates G-code to set the feed rate mode (G94 or G95).
+   * @param {94 | 95} mode - The desired feed rate mode (G94 or G95).
    * @param {CommandOptions} [options] - Optional settings for command generation.
    */
-  public UseMillimeters(options?: CommandOptions): void {
-    this.put(this._machine.setUnitSystem(21, options?.forcePrint));
-  }
-
-  /**
-   * @method UseInches
-   * @description Generates G-code to set the machine to use inches (G20).
-   * @param {CommandOptions} [options] - Optional settings for command generation.
-   */
-  public UseInches(options?: CommandOptions): void {
-    this.put(this._machine.setUnitSystem(20, options?.forcePrint));
+  public SetFeedRateMode(mode: 94 | 95, options?: CommandOptions): void {
+    this.put(this._machine.setFeedRateMode(mode, options?.forcePrint), options);
   }
 }
