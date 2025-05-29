790cec15e66fee0258d3853768855624bfab4185
komeil <catinrage@gmail.com>
2025-05-29 15:53:17 +0330

    Refactor Builder class to introduce File management and enhance G-code generation
    - Added a new File class to manage individual G-code files, encapsulating file-specific options and behaviors.
    - Updated the Builder class to handle multiple files, including main and subprograms, with methods for creating and managing these files.
    - Enhanced the constructor of both Builder and File classes to accept new options, improving configurability.
    - Refactored G-code command methods to utilize the current file context, ensuring proper command handling and flushing.
    - Introduced a mainFileName option in BuilderOptions for customizable main file naming.

--- src/lib/builder.ts
+++ src/lib/builder.ts
@@ -1,5 +1,6 @@
-import type { CommandsType } from '$src/types';
+import type { CommandsType, DeepPartial, EventsType } from '$src/types';
 import { Machine } from './machine';
+import type { Event } from './program';
 
 /**
  * Interface for command options
@@ -20,6 +21,10 @@ export interface CommandOptions {
  */
 export type BuilderOptions = {
   /**
+   * The name of the main file, default is 'Setup.MPF'
+   */
+  mainFileName: string;
+  /**
    * Settings for line numbering in generated G-code
    */
   numbering: {
@@ -38,32 +43,30 @@ export type BuilderOptions = {
   };
 };
 
-/**
- * @class Builder
- * @description A class to construct a G-code program by accumulating G-code commands,
- * formatting them with line numbers (N-words), and managing the machine state
- * via the Machine class.
- */
-export class Builder {
+export type FileOptions = {
+  numbering: BuilderOptions['numbering'];
+};
+
+export class File {
+  /**
+   * @private
+   * @property _builder
+   * @description The builder instance.
+   */
+  private readonly _builder: Builder;
+
   /**
    * @private
    * @property _options
-   * @description The options for the Program.
+   * @description The options for the file.
    */
-  private readonly _options: BuilderOptions = {
+  private readonly _options: FileOptions = {
     numbering: {
       enabled: true,
       start: 10,
       increment: 10,
     },
   };
-
-  /**
-   * @private
-   * @property _machine
-   * @description An instance of the Machine class to manage CNC machine state and generate G-code words.
-   */
-  private _machine = new Machine();
 
   /**
    * @private
@@ -88,20 +91,34 @@ export class Builder {
 
   /**
    * @constructor
-   * @description Initializes a new Builder instance with the given options
-   * @param {BuilderOptions} [options] - Optional configuration options
+   * @description Initializes a new File instance.
+   * @param {Builder} builder - The builder instance.
+   * @param {string} name - The name of the file.
+   * @param {DeepPartial<FileOptions>} [options] - Optional settings for the file.
    */
-  constructor(options?: BuilderOptions) {
+  constructor(
+    builder: Builder,
+    name: string,
+    options?: DeepPartial<FileOptions>,
+  ) {
+    this.name = name;
+    this._builder = builder;
     this._options = {
       numbering: {
-        enabled: options?.numbering.enabled ?? this._options.numbering.enabled,
-        start: options?.numbering.start ?? this._options.numbering.start,
+        enabled: options?.numbering?.enabled ?? this._options.numbering.enabled,
+        start: options?.numbering?.start ?? this._options.numbering.start,
         increment:
-          options?.numbering.increment ?? this._options.numbering.increment,
+          options?.numbering?.increment ?? this._options.numbering.increment,
       },
     };
-    this._currentLineNumber = this._options.numbering.start;
   }
+
+  /**
+   * @private
+   * @property name
+   * @description The name of the file.
+   */
+  public readonly name: string;
 
   /**
    * @property {string} gcode
@@ -147,6 +164,174 @@ export class Builder {
       this._currentLineNumber += this._options.numbering.increment;
     }
   }
+}
+
+/**
+ * @class Builder
+ * @description A class to construct a G-code program by accumulating G-code commands,
+ * formatting them with line numbers (N-words), and managing the machine state
+ * via the Machine class.
+ */
+export class Builder {
+  /**
+   * @private
+   * @property _options
+   * @description The options for the Program.
+   */
+  private readonly _options: BuilderOptions = {
+    mainFileName: 'Setup.MPF',
+    numbering: {
+      enabled: true,
+      start: 10,
+      increment: 10,
+    },
+  };
+
+  /**
+   * @private
+   * @property _files
+   * @description An array to store the files.
+   */
+  private readonly _files: File[] = [];
+
+  /**
+   * @private
+   * @property _mainFile
+   * @description The main file.
+   */
+  private _mainFile: File;
+
+  /**
+   * @private
+   * @property _currentFile
+   * @description The current file.
+   */
+  private _currentFile: File;
+
+  /**
+   * @private
+   * @property _machine
+   * @description An instance of the Machine class to manage CNC machine state and generate G-code words.
+   */
+  private _machine: Machine;
+
+  /**
+   * @constructor
+   * @description Initializes a new Builder instance with the given options
+   * @param {BuilderOptions} [options] - Optional configuration options
+   */
+  constructor(options?: DeepPartial<BuilderOptions>) {
+    this._options = {
+      mainFileName: options?.mainFileName ?? this._options.mainFileName,
+      numbering: {
+        enabled: options?.numbering?.enabled ?? this._options.numbering.enabled,
+        start: options?.numbering?.start ?? this._options.numbering.start,
+        increment:
+          options?.numbering?.increment ?? this._options.numbering.increment,
+      },
+    };
+    this._mainFile = new File(this, this._options.mainFileName);
+    this._files.push(this._mainFile);
+    this._currentFile = this._mainFile;
+    this._machine = new Machine(this);
+  }
+
+  /**
+   * @public
+   * @property currentEvent
+   * @description The current event being processed.
+   */
+  public currentEvent: Event<keyof EventsType> | null = null;
+
+  /**
+   * @public
+   * @property currentEventListenerIndex
+   * @description The index of the current event listener being processed.
+   */
+  public currentEventListenerIndex: number = 0;
+
+  /**
+   * @method build
+   * @description Builds the G-code program.
+   * @returns {Array<{ file: string; code: string }>} An array of objects containing the file name and the G-code code.
+   */
+  public build(): {
+    file: string;
+    code: string;
+  }[] {
+    return this._files.map((file) => ({
+      file: file.name,
+      code: file.gcode,
+    }));
+  }
+
+  /**
+   * @method put
+   * @description Adds a G-code word or segment (e.g., "G00", "X100", "M03") to the current line being built.
+   * Sections should be valid G-code words. Empty strings will be ignored by `flush`.
+   * @param {string} section - The G-code segment to add to the current line.
+   * @param {boolean} [flush] - Whether to flush the current line after adding the section.
+   */
+  public put(section: string, flush?: boolean): void {
+    this._currentFile.put(section, flush);
+  }
+
+  /**
+   * @method flush
+   * @description Finalizes the current G-code line. If the `_currentLine` buffer contains any words,
+   * it prepends the line number (N-word), joins the words with spaces, trims whitespace,
+   * adds it to the `_lines` array, clears the `_currentLine` buffer, and increments the `_lineNumber`.
+   */
+  public flush(): void {
+    this._currentFile.flush();
+  }
+
+  /**
+   * @method NewSubProgram
+   * @description Creates a new subprogram with the given name.
+   * @param {string} name - The name of the new subprogram.
+   * @throws {Error} If the builder is currently in a subprogram.
+   */
+  public NewSubProgram(name: string): void {
+    // If builder is not currently in the main file, throw and error as its only possible to create new files in the main file
+    if (this._currentFile !== this._mainFile) {
+      throw new Error('Cannot create new files in non-main (MPF) files');
+    }
+    this._files.push(new File(this, name + '.SPF'));
+    this._currentFile = this._files[this._files.length - 1];
+  }
+
+  /**
+   * @method EndSubProgram
+   * @description Ends the current subprogram.
+   * @throws {Error} If the builder is currently in the main file.
+   */
+  public EndSubProgram(): void {
+    // If builder is currently in the main file, throw and error as its only possible to end the main file
+    if (this._currentFile === this._mainFile) {
+      throw new Error('Cannot end the main (MPF) file');
+    }
+
+    this._currentFile = this._mainFile;
+  }
+
+  /**
+   * @method Call
+   * @description Generates G-code to call a subprogram (SPF).
+   * @param {string} name - The name of the subprogram to call.
+   */
+  public Call(name: string): void {
+    this._currentFile.put(`CALL "${name}.SPF"`, true);
+  }
+
+  /**
+   * @method ExtCall
+   * @description Generates G-code to call a subprogram (SPF) from an external source (e.g., a USB drive).
+   * @param {string} name - The name of the subprogram to call.
+   */
+  public ExtCall(name: string): void {
+    this._currentFile.put(`EXTCALL "${name}.SPF"`);
+  }
 
   /**
    * @method Rapid
@@ -157,10 +342,12 @@ export class Builder {
    * @example 'G0 X100 Y50 Z10'
    */
   public Rapid(params: CommandsType['Rapid'], options?: CommandOptions): void {
-    this.put(this._machine.setMotionMode(0, options?.forcePrint));
-    this.put(this._machine.setPosition(params, options?.forcePrint));
+    this._currentFile.put(this._machine.setMotionMode(0, options?.forcePrint));
+    this._currentFile.put(
+      this._machine.setPosition(params, options?.forcePrint),
+    );
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 
@@ -174,10 +361,12 @@ export class Builder {
    * @example 'G1 X100 Y50 Z10'
    */
   public Line(params: CommandsType['Line'], options?: CommandOptions): void {
-    this.put(this._machine.setMotionMode(1, options?.forcePrint));
-    this.put(this._machine.setPosition(params, options?.forcePrint));
+    this._currentFile.put(this._machine.setMotionMode(1, options?.forcePrint));
+    this._currentFile.put(
+      this._machine.setPosition(params, options?.forcePrint),
+    );
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 
@@ -191,9 +380,11 @@ export class Builder {
     speed: CommandsType['SetSpindleSpeed'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.setSpindleSpeed(speed, options?.forcePrint));
+    this._currentFile.put(
+      this._machine.setSpindleSpeed(speed, options?.forcePrint),
+    );
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 
@@ -208,9 +399,11 @@ export class Builder {
     direction: CommandsType['SetSpindleDirection'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.setSpindleDirection(direction, options?.forcePrint));
+    this._currentFile.put(
+      this._machine.setSpindleDirection(direction, options?.forcePrint),
+    );
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 
@@ -225,9 +418,11 @@ export class Builder {
     feedRate: CommandsType['SetFeedRate'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.setFeedRate(feedRate, options?.forcePrint));
+    this._currentFile.put(
+      this._machine.setFeedRate(feedRate, options?.forcePrint),
+    );
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 
@@ -242,16 +437,18 @@ export class Builder {
     toolNumber: CommandsType['SelectTool'],
     options?: CommandOptions,
   ): void {
-    this.put(this._machine.selectTool(toolNumber, options?.forcePrint));
+    this._currentFile.put(
+      this._machine.selectTool(toolNumber, options?.forcePrint),
+    );
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 
   public ChangeTool(options?: CommandOptions): void {
-    this.put('M6');
+    this._currentFile.put('M6');
     if (!options?.skipNewLine) {
-      this.flush();
+      this._currentFile.flush();
     }
   }
 }
